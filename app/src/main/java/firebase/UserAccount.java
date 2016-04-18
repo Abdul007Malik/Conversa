78db7dcab94b451d8876e12e6d057a9e09e60437
Abdul Malik <abdul007malik@hotmail.com>
2016-04-18 22:40:36 +0530

    login firebase

--- app/src/main/java/firebase/UserAccount.java
+++ app/src/main/java/firebase/UserAccount.java
@@ -5,17 +5,16 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.FirebaseException;
 
+import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by AM on 13-04-2016.
- */
+
 public class UserAccount {
 
     Firebase firebase;
     String username;
     String password;
-    Boolean accountStatus, loginStatus; //True if task is successfully completed or not
+    Boolean accountStatus, status; //True if task is successfully completed or not
 
     /*
     * This method creates user account but not authenticate, for that userLogin() method is used
@@ -62,19 +61,58 @@ public class UserAccount {
             @Override
             public void onAuthenticated(AuthData authData) {
                 System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
-                loginStatus = true;
+                // Authentication just completed successfully :)
+                Map<String, String> map = new HashMap<>();
+                map.put("provider", authData.getProvider());
+                if(authData.getProviderData().containsKey("displayName")) {
+                    map.put("displayName", authData.getProviderData().get("displayName").toString());
+                }
+                firebase.child("users").child(authData.getUid()).setValue(map);
+                status = true;
             }
 
             @Override
-            public void onAuthenticationError(FirebaseError firebaseError) {
+            public void onAuthenticationError(FirebaseError error) {
                 System.out.println("Something went wrong!! \n logging is not possible cannot be created..");
-                loginStatus = false;
+                // Something went wrong :(
+                switch (error.getCode()) {
+                    case FirebaseError.USER_DOES_NOT_EXIST:
+                        // handle a non existing user
+                        System.out.println("either username or password is incorrect ");
+                        break;
+                    case FirebaseError.INVALID_PASSWORD:
+                        // handle an invalid password
+                        System.out.println("password is invalid");
+                        break;
+                    default:
+                        // handle other errors
+                        System.out.println("server is down!!!");
+                        break;
+                }
+                status = false;
             }
         });
 
-        return loginStatus;
+        return status;
     }
 
+/*
+* This method will remove the user from database
+*/
 
+    protected void remove(String username, String password)throws FirebaseException{
+        if(firebase == null)
+            firebase = new Firebase("https://conversa.firebaseio.com");
+        firebase.removeUser(username, password, new Firebase.ResultHandler() {
+            @Override
+            public void onSuccess() {
+                status = true;
+            }
 
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                status = false;
+            }
+        });
+    }
 }
